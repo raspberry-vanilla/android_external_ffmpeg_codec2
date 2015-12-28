LineageOS/android_external_stagefright-plugins

stagefright-plugins: Increase confidence on HEVC mp2p files
AOSP does not handle HEVC video codec in mp2p file.
Add more confidence in FFmpegExtractor to use ffmpeg
to decode this type of file.

Change-Id: I34f19e8d1e05c02e92a42386cb8d039f3e2a680d

extractor/FFmpegExtractor.cpp
@@ -1682,6 +1682,36 @@ static void adjustMPEG4Confidence(AVFormatContext *ic, float *confidence)
     }
 }
 
+static void adjustMPEG2PSConfidence(AVFormatContext *ic, float *confidence)
+{
+    enum AVCodecID codec_id = AV_CODEC_ID_NONE;
+
+    codec_id = getCodecId(ic, AVMEDIA_TYPE_VIDEO);
+    if (codec_id != AV_CODEC_ID_NONE
+            && codec_id != AV_CODEC_ID_H264
+            && codec_id != AV_CODEC_ID_MPEG4
+            && codec_id != AV_CODEC_ID_MPEG1VIDEO
+            && codec_id != AV_CODEC_ID_MPEG2VIDEO) {
+        //the MEDIA_MIMETYPE_CONTAINER_MPEG2TS of confidence is 0.25f
+        ALOGI("[mpeg2ps]video codec(%s), confidence should be larger than MPEG2PSExtractor",
+                avcodec_get_name(codec_id));
+        *confidence = 0.26f;
+    }
+
+    codec_id = getCodecId(ic, AVMEDIA_TYPE_AUDIO);
+    if (codec_id != AV_CODEC_ID_NONE
+            && codec_id != AV_CODEC_ID_AAC
+            && codec_id != AV_CODEC_ID_PCM_S16LE
+            && codec_id != AV_CODEC_ID_PCM_S24LE
+            && codec_id != AV_CODEC_ID_MP1
+            && codec_id != AV_CODEC_ID_MP2
+            && codec_id != AV_CODEC_ID_MP3) {
+        ALOGI("[mpeg2ts]audio codec(%s), confidence should be larger than MPEG2PSExtractor",
+                avcodec_get_name(codec_id));
+        *confidence = 0.261f;
+    }
+}
+
 static void adjustMPEG2TSConfidence(AVFormatContext *ic, float *confidence)
 {
     enum AVCodecID codec_id = AV_CODEC_ID_NONE;
@@ -1773,6 +1803,8 @@ static void adjustConfidenceIfNeeded(const char *mime,
         adjustMPEG4Confidence(ic, confidence);
     } else if (!strcasecmp(mime, MEDIA_MIMETYPE_CONTAINER_MPEG2TS)) {
         adjustMPEG2TSConfidence(ic, confidence);
+    } else if (!strcasecmp(mime, MEDIA_MIMETYPE_CONTAINER_MPEG2PS)) {
+        adjustMPEG2PSConfidence(ic, confidence);
     } else if (!strcasecmp(mime, MEDIA_MIMETYPE_CONTAINER_MATROSKA)) {
         adjustMKVConfidence(ic, confidence);
     } else {

raspberry-vanilla/android_external_ffmpeg_codec2

extractor/FFmpegExtractor.cpp
@@ -1710,6 +1710,36 @@ static void adjustMPEG4Confidence(AVFormatContext *ic, float *confidence)
     }
 }
 
+static void adjustMPEG2PSConfidence(AVFormatContext *ic, float *confidence)
+{
+    enum AVCodecID codec_id = AV_CODEC_ID_NONE;
+
+    codec_id = getCodecId(ic, AVMEDIA_TYPE_VIDEO);
+    if (codec_id != AV_CODEC_ID_NONE
+            && codec_id != AV_CODEC_ID_H264
+            && codec_id != AV_CODEC_ID_MPEG4
+            && codec_id != AV_CODEC_ID_MPEG1VIDEO
+            && codec_id != AV_CODEC_ID_MPEG2VIDEO) {
+        //the MEDIA_MIMETYPE_CONTAINER_MPEG2TS of confidence is 0.25f
+        ALOGI("[mpeg2ps]video codec(%s), confidence should be larger than MPEG2PSExtractor",
+                avcodec_get_name(codec_id));
+        *confidence = 0.26f;
+    }
+
+    codec_id = getCodecId(ic, AVMEDIA_TYPE_AUDIO);
+    if (codec_id != AV_CODEC_ID_NONE
+            && codec_id != AV_CODEC_ID_AAC
+            && codec_id != AV_CODEC_ID_PCM_S16LE
+            && codec_id != AV_CODEC_ID_PCM_S24LE
+            && codec_id != AV_CODEC_ID_MP1
+            && codec_id != AV_CODEC_ID_MP2
+            && codec_id != AV_CODEC_ID_MP3) {
+        ALOGI("[mpeg2ps]audio codec(%s), confidence should be larger than MPEG2PSExtractor",
+                avcodec_get_name(codec_id));
+        *confidence = 0.26f;
+    }
+}
+
 static void adjustMPEG2TSConfidence(AVFormatContext *ic, float *confidence)
 {
     enum AVCodecID codec_id = AV_CODEC_ID_NONE;
@@ -1801,6 +1831,8 @@ static void adjustConfidenceIfNeeded(const char *mime,
         adjustMPEG4Confidence(ic, confidence);
     } else if (!strcasecmp(mime, MEDIA_MIMETYPE_CONTAINER_MPEG2TS)) {
         adjustMPEG2TSConfidence(ic, confidence);
+    } else if (!strcasecmp(mime, MEDIA_MIMETYPE_CONTAINER_MPEG2PS)) {
+        adjustMPEG2PSConfidence(ic, confidence);
     } else if (!strcasecmp(mime, MEDIA_MIMETYPE_CONTAINER_MATROSKA)) {
         adjustMKVConfidence(ic, confidence);
     } else if (!strcasecmp(mime, MEDIA_MIMETYPE_CONTAINER_DIVX)) {