85ac83b43b8faa21e881921a34c1a6086772c2e4
Konsta <konsta09@gmail.com>
2025-10-23 22:52:34 +0300

    move seccomp policy into apex

--- main.cpp
+++ main.cpp
@@ -27,16 +27,15 @@
 
 using namespace ::aidl::android::hardware::media::c2;
 
-// This is the absolute on-device path of the prebuild_etc module
-// "android.hardware.media.c2-default-seccomp_policy" in Android.bp.
+// This is the absolute on-device path of the prebuilt_etc module
+// "android.hardware.media.c2-ffmpeg-default-seccomp_policy" in Android.bp.
 static constexpr char kBaseSeccompPolicyPath[] =
-        "/vendor/etc/seccomp_policy/"
-        "android.hardware.media.c2-default-seccomp_policy";
+        "/apex/com.android.hardware.media.c2.ffmpeg/etc/seccomp_policy/"
+        "android.hardware.media.c2-ffmpeg-default-seccomp_policy";
 
 // Additional seccomp permissions can be added in this file.
-// This file does not exist by default.
 static constexpr char kExtSeccompPolicyPath[] =
-        "/vendor/etc/seccomp_policy/"
+        "/apex/com.android.hardware.media.c2.ffmpeg/etc/seccomp_policy/"
         "android.hardware.media.c2-ffmpeg-extended-seccomp_policy";
 
 int main() {
